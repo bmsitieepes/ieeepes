019df7b14df68622b9bdbe12493f885732267531
SumanGs05 <sumanog2005@gmail.com>
2025-05-17 22:57:07 +0530

    image bug fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ieeepes/', // IMPORTANT: Make sure 'ieeepes' matches your repository name exactly
+  base: "/ieeepes/", // IMPORTANT: Make sure 'ieeepes' matches your repository name exactly
 });
